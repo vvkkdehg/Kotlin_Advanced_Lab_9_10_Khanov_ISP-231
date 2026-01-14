87630ff7f43d0a88deba209e8276f1f7be4dd0c3
Vlados <hanovvladik@mail.ru>
2026-01-14 15:35:10 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -12,16 +12,46 @@ data class Item(
         return "ID: $id | name: $name | quantity: $quantity"
     }
 }
+
+abstract class Human(val name: String) {
+    abstract var age: Int
+    abstract fun hello()
+}
+class Person(name: String, override var age : Int): Human(name) {
+    override fun hello(){
+        println("My name is $name")
+    }
+}
+
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+
+class Rectangle(val width: Float, val height: Float) : Figure() {
+    override fun perimeter(): Float {
+        return 2 * (width + height)
+    }
+
+    override fun area(): Float {
+        return width * height
+    }
+}
+
 fun main() {
 //    println(age)
 //    age = 55
 //    println(age)
 //    age = -43
 //    println(age)
-    val sword = Item(1, "Sword", 1)
-    val betterSword = sword.copy(quantity = 2, name = "Better Sword")
-    println(sword.toString())
-    println(betterSword.toString())
-    val(id, name, quantity) = betterSword
-    println("$id | $name | $quantity")
+//    val sword = Item(1, "Sword", 1)
+//    val betterSword = sword.copy(quantity = 2, name = "Better Sword")
+//    println(sword.toString())
+//    println(betterSword.toString())
+//    val(id, name, quantity) = betterSword
+//    println("$id | $name | $quantity")
+    val vlad: Person = Person("Vlad", 18)
+    val zhenek: Human = Person("Zhenek", 17)
+    vlad.hello()
+    zhenek.hello()
 }
